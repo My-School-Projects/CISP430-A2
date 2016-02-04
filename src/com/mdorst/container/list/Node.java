295ce78e2415aaa0042be8066e75677e95a6631a
Michael Dorst <code@mikefdorst.com>
2016-02-03 18:02:14 -0800

    Add static methods to Node (incomplete)

--- src/com/mdorst/container/list/Node.java
+++ src/com/mdorst/container/list/Node.java
@@ -11,10 +11,33 @@ package com.mdorst.container.list;
  * + data : T
  * + next : Node
  * + prev : Node
+ *
+ * <<static>> + insertNext(Node, Node)
+ * <<static>> + swapNext(Node)
  */
 
 class Node<T> {
     public T data;
     public Node<T> next;
     public Node<T> prev;
+
+    public static <T> void insertNext(Node<T> loc, Node<T> obj) {
+        if (loc == null || obj == null) {
+            throw new NullPointerException();
+        }
+        Node<T> temp = loc.next;
+        loc.next = obj;
+        temp.prev = obj;
+        obj.prev = loc;
+        obj.next = temp;
+    }
+
+    public static <T> void swapNext(Node<T> node) {
+        if (node == null || node.next == null) {
+            throw new NullPointerException();
+        }
+        node.prev.next = node.next;
+        node.next.next.prev = node;
+        // TODO: finish
+    }
 }
