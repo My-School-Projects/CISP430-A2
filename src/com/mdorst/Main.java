7331e92fa957d4cc5fd6c586fd2d02b1d70a1d16
Michael Dorst <code@mikefdorst.com>
2016-03-05 16:23:19 -0800

    Add more tests

--- src/com/mdorst/Main.java
+++ src/com/mdorst/Main.java
@@ -2,6 +2,8 @@ package com.mdorst;
 
 import com.mdorst.container.list.ListCollection;
 
+import java.util.Arrays;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -11,12 +13,17 @@ public class Main {
         test.assertTrue(list.isEmpty(), "list.isEmpty() should be true after construction");
         test.assertFalse(list.contains(5), "list.contains(5) should be false after construction");
         test.assertTrue(list.containsAll(list), "list.containsAll(list) should always be true");
-        test.assertFalse(list.iterator().hasNext(), "list.iterator.hasNext() should be false for an empty list");
+        test.assertFalse(list.iterator().hasNext(), "list.iterator().hasNext() should be false for an empty list");
+        test.assertTrue(Arrays.deepEquals(list.toArray(), new Object[] {}), "list.toArray() should be an empty array");
         list.add(5);
         test.assertEqual(list.size(), 1, "list.size() should be 1 after calling add()");
+        test.assertFalse(list.isEmpty(), "list.isEmpty() should be false after construction");
         test.assertTrue(list.contains(5), "list.contains(5) should be true after calling add(5)");
         test.assertTrue(list.containsAll(list), "list.containsAll(list) should always be true");
-        test.assertTrue(list.iterator().hasNext(), "list.iterator.hasNext() should be true for a non-empty list");
+        test.assertTrue(list.iterator().hasNext(), "list.iterator().hasNext() should be true for a non-empty list");
+        test.assertEqual(list.iterator().next(), 5, "list.iterator().next() should be 5");
+        test.assertEqual(list.toArray().length, 1, "list.toArray() should return an array of length 1");
+        test.assertTrue(Arrays.deepEquals(list.toArray(), new Object[] {5}), "list.toArray should == {5}");
         test.done();
     }
 }
