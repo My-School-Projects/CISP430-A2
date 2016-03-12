856398d983ee38a54b5466c84fa12d8d301ff863
Michael Dorst <code@mikefdorst.com>
2016-03-11 19:37:48 -0800

    Add verbose mode to TestRunner

--- src/com/mdorst/Main.java
+++ src/com/mdorst/Main.java
@@ -11,6 +11,7 @@ public class Main {
 
     public static void main(String[] args) {
         TestRunner test = new TestRunner();
+        test.verbose = true;
         /**
          * ==============
          * LinkedList
--- src/com/mdorst/TestRunner.java
+++ src/com/mdorst/TestRunner.java
@@ -8,14 +8,24 @@ import java.util.Objects;
 /**
  * This class serves as a simple testing framework.
  * <p>
- * Calls to {@code assert*} are tracked, and error messages are stored
- * for a later call to {@code done()}, which will either print
- * "Passed!" or "Failed." and a list of failed assertions if applicable.
+ * Calls to {@code assert*} are tracked, and expectations are stored
+ * for a later call to {@code done()}, which print the number of passed and
+ * failed tests. The expectations of the failed tests will be displayed in red.
+ * If {@code verbose == true}, the expectations of the passed tests will be
+ * displayed in green.
  */
 public class TestRunner {
     private List<String> log;
     private PrintStream stream;
-    private int testCount;
+    private int passCount;
+    private int failCount;
+    /**
+     * If {@code false}, only expectations for failed tests will be
+     * displayed. If {@code true}, all expectations will be
+     * displayed.
+     * Default: {@code false}
+     */
+    public boolean verbose = false;
 
     private class Color {
         public static final String RED = "\u001B[31m";
@@ -23,42 +33,50 @@ public class TestRunner {
         public static final String RESET = "\u001B[0m";
     }
 
-    public void assertTrue(boolean assertion, String error) {
+    public void assertTrue(boolean assertion, String expectation) {
         if (assertion) {
             stream.print(Color.GREEN + "." + Color.RESET);
+            if (verbose)
+                log.add(Color.GREEN + expectation + Color.RESET);
+            passCount++;
         } else {
             stream.print(Color.RED + "F" + Color.RESET);
-            log.add(Color.RED + error + Color.RESET);
+            log.add(Color.RED + expectation + Color.RESET);
+            failCount++;
         }
-        testCount++;
     }
 
     public void assertTrue(boolean assertion) {
         assertTrue(assertion, "Invalid assertion");
     }
 
-    public void assertFalse(boolean assertion, String error) {
-        assertTrue(!assertion, error);
+    public void assertFalse(boolean assertion, String expectation) {
+        assertTrue(!assertion, expectation);
     }
 
     public void assertFalse(boolean assertion) {
         assertTrue(!assertion);
     }
 
-    public void assertEqual(Object o1, Object o2, String error) {
-        assertTrue(Objects.equals(o1, o2), error);
+    public void assertEqual(Object o1, Object o2, String expectation) {
+        assertTrue(Objects.equals(o1, o2), expectation);
     }
 
     public void assertEqual(Object o1, Object o2) {
-        assertTrue(Objects.equals(o1, o2), o1 + " != " + o2);
+        if (Objects.equals(o1, o2)) {
+            assertTrue(true, o1 + " == " + o2);
+        } else {
+            assertTrue(false, o1 + " != " + o2);
+        }
     }
 
     /**
-     * Prints "n passed, n failed", and displays any failed test messages
+     * Prints "n passed, n failed", and displays any failed test expectations.
+     * Displays passed test expectations if {@code verbose == true}
      */
     public void done() {
-        stream.println("\n" + Color.GREEN + (testCount - log.size()) + " passed, " +
-                Color.RED + log.size() + " failed." + Color.RESET);
+        stream.println("\n" + Color.GREEN + passCount + " passed, " +
+                Color.RED + failCount + " failed." + Color.RESET);
         log.forEach(stream::println);
     }
 
