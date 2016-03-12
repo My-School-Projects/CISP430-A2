9592bf9427cec6efc1bc824eff2701a7145e9747
Michael Dorst <code@mikefdorst.com>
2016-03-12 03:23:02 -0800

    Write tests for size() and remove()

--- src/com/mdorst/Main.java
+++ src/com/mdorst/Main.java
@@ -141,6 +141,25 @@ public class Main {
             test.assertEqual(iter.next(), 10.0, "next()");
             test.assertFalse(iter.hasNext(), "hasNext()");
             test.assertEqual(iter.next(), null, "next()");
+            test.assertEqual(list.size(), 5, "size()");
+            test.assertTrue(list.remove(5.0), "remove(5.0)");
+            test.assertEqual(list.size(), 4, "size()");
+            test.assertFalse(list.remove(5.0), "remove(5.0)");
+            test.assertEqual(list.size(), 4, "size()");
+            test.assertTrue(list.remove(1.5), "remove(1.5)");
+            test.assertEqual(list.size(), 3, "size()");
+            test.log("Reset iterator");
+            iter = list.iterator();
+            test.assertEqual(iter.next(), 2.0, "next()");
+            test.assertEqual(iter.next(), 4.9, "next()");
+            test.assertEqual(iter.next(), 10.0, "next()");
+            test.assertFalse(iter.hasNext(), "hasNext()");
+            test.assertEqual(iter.next(), null, "next()");
+            test.assertTrue(list.remove(4.9), "remove(4.9)");
+            test.assertTrue(list.remove(10.0), "remove(10.0)");
+            test.assertTrue(list.remove(2.0), "remove(2.0)");
+            test.assertEqual(list.size(), 0, "size()");
+            test.assertFalse(list.iterator().hasNext(), "iterator().hasNext()");
         }
         {
             class MagicBox {
@@ -175,11 +194,15 @@ public class Main {
             test.log("push(new MagicBox(new MagicBox(3.14))");
             stack.push(new MagicBox(new MagicBox(3.14)));
             test.assertEqual(((MagicBox)stack.top().open()).open(), 3.14, "top().open().open()");
+            test.assertEqual(stack.size(), 3, "size()");
             test.assertEqual(((MagicBox)stack.pop().open()).open(), 3.14, "pop().open().open()");
+            test.assertEqual(stack.size(), 2, "size()");
             test.assertEqual(stack.pop().open(), "hello", "pop().open()");
             test.assertEqual(stack.pop().open(), 5, "pop().open()");
+            test.assertEqual(stack.size(), 0, "size()");
             test.assertEqual(stack.pop(), null, "pop()");
             test.assertEqual(stack.pop(), null, "pop()");
+            test.assertEqual(stack.size(), 0, "size()");
             test.log("push(new MagicBox(23.5f))");
             stack.push(new MagicBox(23.5f));
             test.assertEqual(stack.pop().open(), 23.5f, "pop().open()");
@@ -220,9 +243,12 @@ public class Main {
             test.assertEqual(iter.next(), "what's up?", "next()");
             test.assertFalse(iter.hasNext(), "hasNext()");
             test.assertEqual(iter.next(), null, "next()");
+            test.assertEqual(queue.size(), 3, "size()");
             test.assertEqual(queue.dequeue(), "hello", "dequeue()");
+            test.assertEqual(queue.size(), 2, "size()");
             test.assertEqual(queue.dequeue(), "goodbye", "dequeue()");
             test.assertEqual(queue.dequeue(), "what's up?", "dequeue()");
+            test.assertEqual(queue.size(), 0, "size()");
             test.assertEqual(queue.dequeue(), null, "dequeue()");
             test.assertEqual(queue.dequeue(), null, "dequeue()");
             test.log("enqueue(\"just in case...\")");
@@ -230,6 +256,7 @@ public class Main {
             test.assertEqual(queue.front(), "just in case...", "front()");
             test.assertEqual(queue.dequeue(), "just in case...", "dequeue()");
             test.assertEqual(queue.dequeue(), null, "dequeue()");
+            test.assertEqual(queue.size(), 0, "size()");
         }
         {
             test.log("====================");
@@ -261,14 +288,18 @@ public class Main {
             test.log("enqueue(1f)");
             queue.enqueue(1f);
             test.assertEqual(queue.front(), 1f, "front()");
+            test.assertEqual(queue.size(), 5, "size()");
             test.assertEqual(queue.dequeue(), 1f, "dequeue()");
+            test.assertEqual(queue.size(), 4, "size()");
             test.assertEqual(queue.dequeue(), 2f, "dequeue()");
             test.assertEqual(queue.dequeue(), 3f, "dequeue()");
             test.assertEqual(queue.front(), 4f, "front()");
             test.assertEqual(queue.dequeue(), 4f, "dequeue()");
             test.assertEqual(queue.dequeue(), 5f, "dequeue()");
+            test.assertEqual(queue.size(), 0, "size()");
             test.assertEqual(queue.front(), null, "front()");
             test.assertEqual(queue.dequeue(), null, "dequeue()");
+            test.assertEqual(queue.size(), 0, "size()");
         }
         test.done();
     }
